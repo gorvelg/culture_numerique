f03b05d682ebe3938f0aa0a967f5260c42e3e4ed
gorvelg <ggorvel@gmail.com>
2023-04-15 19:44:26 +0200

    ajout du texte + ajout lien

--- technique.php
+++ technique.php
@@ -23,13 +23,16 @@
         <h2>Démarche technique</h2>
               <!-- Démarche -->
         <div class="article-contain">
-            <h3></h3>
-            <p>Tout d'abord, après avoir pris connaissance du code HTML, nous avons décidé de revoir la structure afin d'optimiser le SEO et le CSS. Nous voulions un design 
-                épuré et simple, qui mette en avant les articles. Cela favorisait l'accessibilité du site, et optimisait le parcours utilisateur, en diminuant le nombre d'étapes.
-                Notre premier but était de rendre avant tout le site accessible, et nous avons opté pour un développement en "Mobile First". En effet, on privilégiant cette approche
-
-            </p>
-
+            <h3>Usage du Flat Design</h3> 
+            <p>Tout d'abord, après avoir pris connaissance du code HTML, nous avons décidé de revoir la structure afin d'optimiser le SEO et le CSS. Nous voulions un design épuré et simple (Flat Design), qui mette en avant les articles. Cela favorise l'accessibilité du site, optimise le parcours utilisateur (en diminuant le nombre d'étapes.) et réduit la consommation à travers des ressources inutiles.</p>
+            <h3>Technologies adaptées</h3>
+            <p>Pour réduire la consommation d'énergie, nous avons choisi des technologies peu consommatrices d'énergie, telles que le HTML et le CSS, plutôt que des technologies plus gourmandes. Toutefois, nous avons utilisé du PHP et du Javascript en faible quantité, en prenant soin de ne pas surcharger le code avec des fonctionnalités complexes et superflues.</p>
+            <h3>Mobile First</h3> 
+            <p>Notre premier but était de rendre avant tout le site accessible, et nous avons opté pour un développement en "Mobile First". En effet, en privilégiant cette approche, nous pouvions sélectionner et adapter nos ressources en fonction des besoins.</p> 
+            <h3>Compression des images</h3> 
+            <p>Ensuite, pour minimiser les émissions de carbone, nous avons compressé et converti en .webp (un format optimisé pour le web) les images afin de réduire drastiquement leurs poids (certaines sont passées de 900 ko à 40 ko) et leur impact sur la bande passante.</p> 
+            <h3>Un code épuré et compressé</h3>
+            <p>Toujours dans le but de réduire l’impact environnemental et minimiser la quantité de données transférées lors des chargements de page, nous avons optimisé notre code HTML et créé des variables dans notre CSS afin d’éviter les répétitions. De plus, le code a été minimisé avec un outil qui permet de supprimer tous les caractères inutiles.</p>
         </div>
     </div>
 
--- templates/header.php
+++ templates/header.php
@@ -11,15 +11,15 @@
     <i class="fas fa-bars"></i>
     <div class="modal">
       <div class="navbar-mobile-list">
-        <a href="#">ACCUEIL</a>
-        <a href="#">ARTICLES</a>
-        <a href="#">DÉMARCHE TECHNIQUE</a>
+        <a href="index.php">ACCUEIL</a>
+        <a href="article.php">ARTICLES</a>
+        <a href="technique.php">DÉMARCHE TECHNIQUE</a>
       </div>
     </div>
   </header>
   <!-- fin navbar mobile -->
     <header class="navbar">
-      <a href="">
+      <a href="index.php">
         <div class="logo-box">
             <div>
                 <img class="logo" src="images/logo.png" alt="logo rond vert">
@@ -32,20 +32,20 @@
           <div>
               <ul>
                 <li>
-                  <a href="#">Accueil</a>
+                  <a href="index.php">Accueil</a>
                 </li>
                 <li> <!-- https://www.w3schools.com/howto/howto_js_collapsible.asp -->
                   <a class="collapsible" href="#" >
                     Articles
                   </a>
                   <ul class="content">
-                    <li><a href="#">Le numérique nous rend-il plus intelligent ?</a></li>
+                    <li><a href="article.php">Le numérique nous rend-il plus intelligent ?</a></li>
                     <li><a href="#">Le numérique permet-il de réduire nos impacts environnementaux ?</a></li>
                     <li><a href="#">...</a></li>
                   </ul>
                 </li>
                 <li>
-                  <a href="technique.html">Démarche technique</a>
+                  <a href="technique.php">Démarche technique</a>
                 </li>
               </ul>
           </div>
